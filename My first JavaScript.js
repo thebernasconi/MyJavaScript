e3963b8468717b74605331eeb1ee2b0591b27c1c
thebernasconi <thebernasconi@gmail.com>
2024-07-22 14:58:35 -0300

    Add files via upload

---
+++ My first JavaScript.js
@@ -0,0 +1,12 @@
+const a=10;
+const b=3;
+console.log ("This Is an IF with only one condition");
+if (a>b){
+    console.log ("a is larger than b");
+}else if (a==b){
+    console.log ("a is equal to b");
+}else if (a<b){
+    console.log ("a is smaller than b");
+}else if (b<a){
+    console.log ("b is smaller than a");
+}
